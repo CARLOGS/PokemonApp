f0c2e4d1bbbc52638eaafa6e8768469e313a663c
CarloGS <carlogs@hotmail.com>
2025-03-01 22:55:56 -0600

    Implementación de la funcionalidad Modal Paso de parámetros al modal

---
+++ PokemonApp/Model/Pokemon.swift
@@ -0,0 +1,8 @@
+//
+//  Pokemon.swift
+//  PokemonApp
+//
+//  Created by CARLO GARCIA on 01/03/25.
+//
+
+import Foundation
---
+++ PokemonApp/View/PokemonCell.swift
@@ -0,0 +1,23 @@
+//
+//  PokemonCell.swift
+//  PokemonApp
+//
+//  Created by CARLO GARCIA on 01/03/25.
+//
+
+import UIKit
+
+class PokemonCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
